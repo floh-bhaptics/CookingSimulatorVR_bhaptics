2765fa321e51bd3d124bb1d5edd22aa99d4b9003
floh-bhaptics <floh@fahrenbergers.de>
2022-03-31 12:37:45 +0200

    Added explosions, standmixer, and grabbable interactions

--- CookingSimulatorVR_bhaptics.cs
+++ CookingSimulatorVR_bhaptics.cs
@@ -20,16 +20,128 @@ namespace CookingSimulatorVR_bhaptics
             tactsuitVr = new TactsuitVR();
             tactsuitVr.PlaybackHaptics("HeartBeat");
         }
-        /*
-        [HarmonyPatch(typeof(VertigoPlayer), "Die", new Type[] { })]
-        public class bhaptics_PlayerDies
+        
+        [HarmonyPatch(typeof(CuttingKnifeV2), "HapticsOnSuccessCut", new Type[] { typeof(BNG.ControllerHand) })]
+        public class bhaptics_KnifeCutSuccess
+        {
+            [HarmonyPostfix]
+            public static void Postfix(BNG.ControllerHand handSide)
+            {
+                tactsuitVr.Handle("CutSuccess", (handSide == BNG.ControllerHand.Right));
+            }
+        }
+
+        [HarmonyPatch(typeof(CuttingKnifeV2), "HapticsOnFailedCut", new Type[] { typeof(BNG.ControllerHand) })]
+        public class bhaptics_KnifeCutFailed
+        {
+            [HarmonyPostfix]
+            public static void Postfix(BNG.ControllerHand handSide)
+            {
+                tactsuitVr.Handle("CutFailed", (handSide == BNG.ControllerHand.Right));
+            }
+        }
+
+        [HarmonyPatch(typeof(CombinedProductPartEvents), "ManageHaptics", new Type[] { typeof(float) })]
+        public class bhaptics_CombineProducts
         {
             [HarmonyPostfix]
             public static void Postfix()
             {
-                tactsuitVr.StopThreads();
+                tactsuitVr.Handle("Combine", true);
+                tactsuitVr.Handle("Combine", false);
             }
         }
-        */
+
+        [HarmonyPatch(typeof(BNG.Grabbable), "OnCollisionEnter")]
+        public class bhaptics_GrabbableCollide
+        {
+            [HarmonyPostfix]
+            public static void Postfix(BNG.Grabbable __instance)
+            {
+                
+                tactsuitVr.Handle("CutSuccess", (__instance.GetPrimaryGrabber().HandSide == BNG.ControllerHand.Right), 0.5f);
+            }
+        }
+
+        [HarmonyPatch(typeof(KitchenTimer), "StartRinging", new Type[] {  })]
+        public class bhaptics_AlarmRing
+        {
+            [HarmonyPostfix]
+            public static void Postfix()
+            {
+                tactsuitVr.StartAlarm();
+            }
+        }
+
+        [HarmonyPatch(typeof(KitchenTimer), "EndRinging", new Type[] { })]
+        public class bhaptics_AlarmStop
+        {
+            [HarmonyPostfix]
+            public static void Postfix()
+            {
+                tactsuitVr.StopAlarm();
+            }
+        }
+
+        [HarmonyPatch(typeof(BlenderEvents), "StartBlender", new Type[] { })]
+        public class bhaptics_BlenderStart
+        {
+            [HarmonyPostfix]
+            public static void Postfix()
+            {
+                tactsuitVr.StartBlender();
+            }
+        }
+
+        [HarmonyPatch(typeof(BlenderEvents), "StopBlender", new Type[] { })]
+        public class bhaptics_BlenderStop
+        {
+            [HarmonyPostfix]
+            public static void Postfix()
+            {
+                tactsuitVr.StopBlender();
+            }
+        }
+
+        [HarmonyPatch(typeof(MixContainer), "StartMix", new Type[] { })]
+        public class bhaptics_MixerStart
+        {
+            [HarmonyPostfix]
+            public static void Postfix()
+            {
+                tactsuitVr.StartBlender();
+            }
+        }
+
+        [HarmonyPatch(typeof(MixContainer), "EndMix", new Type[] { })]
+        public class bhaptics_MixerStop
+        {
+            [HarmonyPostfix]
+            public static void Postfix()
+            {
+                tactsuitVr.StopBlender();
+            }
+        }
+
+        [HarmonyPatch(typeof(PlayerStats), "LevelUp", new Type[] { typeof(int), typeof(bool) })]
+        public class bhaptics_LevelUp
+        {
+            [HarmonyPostfix]
+            public static void Postfix()
+            {
+                tactsuitVr.PlaybackHaptics("LevelUp");
+            }
+        }
+
+        [HarmonyPatch(typeof(Explosive), "Explode", new Type[] { typeof(bool), typeof(bool) })]
+        public class bhaptics_Explode
+        {
+            [HarmonyPostfix]
+            public static void Postfix()
+            {
+                tactsuitVr.PlaybackHaptics("ExplosionBelly");
+            }
+        }
+
     }
 }
--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -23,7 +23,7 @@ using MelonLoader;
 // Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
 [assembly: Guid("6ab96946-7db9-4833-9f33-d14066bb454c")]
 
-[assembly: MelonInfo(typeof(CookingSimulatorVR_bhaptics.CookingSimulatorVR_bhaptics), "CookingSimulatorVR_bhaptics", "1.0.0", "Florian Fahrenberger")]
+[assembly: MelonInfo(typeof(CookingSimulatorVR_bhaptics.CookingSimulatorVR_bhaptics), "CookingSimulatorVR_bhaptics", "1.1.0", "Florian Fahrenberger")]
 [assembly: MelonGame("Game Boom VR", "CookingSimulatorVR")]
 
 // Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
@@ -36,5 +36,5 @@ using MelonLoader;
 // Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
 // indem Sie "*" wie unten gezeigt eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0")]
+[assembly: AssemblyFileVersion("1.1.0")]
